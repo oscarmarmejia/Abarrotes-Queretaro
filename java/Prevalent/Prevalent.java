93312f291e75fbd10c3585c70aeacabb3b7db07a
oscarmarmejia <oscarmarmejia@gmail.com>
2022-03-23 11:01:46 -0400

    Add files via upload

---
+++ java/Prevalent/Prevalent.java
@@ -0,0 +1,12 @@
+package mejia.oscar.tiendisuperv4.Prevalent;
+
+import mejia.oscar.tiendisuperv4.Model.Users;
+
+public class Prevalent {
+
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+
+}
